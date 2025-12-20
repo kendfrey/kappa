459bdc3346df0fd4518d60e3b20d2a2c4710aea6
Kendall Frey <kendfrey@gmail.com>
2025-12-20 15:35:31 -0500

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,4 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({ base: "./", plugins: [react()] });
