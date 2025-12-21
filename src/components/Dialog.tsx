c5c21f001756290ada604d6f3490930b4959d7bb
Kendall Frey <kendfrey@gmail.com>
2025-12-21 08:54:34 -0500

    fix styling issue

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -10,7 +10,7 @@ export default function Dialog({ title, width = 400, children, ref }: {
 {
 	return (
 		<dialog ref={ref} {...{ closedby: "any" }}>
-			<form style={{ display: "flex", overflow: "hidden" }}>
+			<form>
 				<div className="flex column" style={{ width }}>
 					<div className="flex" style={{ justifyContent: "space-between", alignItems: "center" }}>
 						<h3 style={{ margin: 0 }}>{title}</h3>
